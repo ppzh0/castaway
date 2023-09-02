89217bcb0ece35d4cf0a8a3f3a65b5cfe43f4134
ppzh0 <narrtor06@gmail.com>
2023-09-02 22:43:10 +0630

    fuck it, it changed a lot :)

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight); // Create a canvas covering the whole viewport
+    noLoop();
+    generateRandomRotatedSquares();
+}
+
+function generateRandomRotatedSquares() {
+    var numSquares = 69;
+    var minSize = 10;
+    var maxSize = 100;
+
+    for (var i = 0; i < numSquares; i++) {
+        var gray = random(20, 125);
+        fill(gray, 100);
+
+        var squareSize = random(minSize, maxSize);
+        var squareX = random(squareSize, width - squareSize);
+        var squareY = random(squareSize, height - squareSize);
+        var rotationAngle = random(TWO_PI);
+
+        push();
+        translate(squareX, squareY);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        rect(0, 0, squareSize * 2, squareSize * 2);
+        pop();
+    }
+}
